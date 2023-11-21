13ed523c2d499c0bb0665e5a5dd98e085143aa26
heeseo06 <heeseo.kim0609@gmail.com>
2023-11-20 20:04:30 -0800

    Co-authored-by: Sam Shin <samshin0714@gmail.com>

--- script.js
+++ script.js
@@ -3,11 +3,25 @@ const weatherApi = {
   baseUrl: 'https://api.openweathermap.org/data/2.5/weather'
 }
 
+let box = document.getElementById("input-box");
+box.addEventListener("keypress", (event) => {
+  if (event.keyCode == 13) {
+    alert("alert!");
+    reset();
+  }
+})
 
 let button = document.getElementById("submit_button");
 button.addEventListener("click", function() {
   alert("alert!");
+  reset();
 });
+
+
+function reset() {
+  let box = document.getElementById("input-box");
+  box.value = "";
+}
 
 // button.addEventListener("click", alert("lakjflasj"));
 // http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=98d4e0ae686e42c873d2a0575825fe83
