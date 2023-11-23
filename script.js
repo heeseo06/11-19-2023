230b34cf3c850b2d8172c43bf4ca0edbdee82af6
heeseo06 <heeseo.kim0609@gmail.com>
2023-11-22 23:04:29 -0800

    Day 5 Changes in javascript

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const weatherApi = {
 let box = document.getElementById("input-box");
 box.addEventListener("keypress", (event) => {
   if (event.keyCode == 13) {
+    // changeHTML(box.value);
     alert("alert!");
     reset();
   }
@@ -13,6 +14,7 @@ box.addEventListener("keypress", (event) => {
 
 let button = document.getElementById("submit_button");
 button.addEventListener("click", function() {
+  // changeHTML(box.value);
   link(box.value);
   reset();
 });
@@ -28,16 +30,45 @@ async function link(city) {
     let content = await fetch(`${weatherApi.baseUrl}?q=${city}&appid=${weatherApi.key}&units=metric`);
     let data = await content.json();
     let temp = data["main"]["temp"];
+    let weatherMain = data["weather"][0]["main"];
+
+    let unixTime = data["dt"];
+    let timeZone = data["timezone"];
+
+    let realTime = unixTime + timeZone;
+
+    let today = new Date((realTime * 1000));
+
+    console.log(today);
+
     let temp_feel = data["main"]["feels_like"];
+    console.log(JSON.stringify(data, null, 2));
     console.log(temp, temp_feel);
-    console.log(data);
+    console.log(weatherMain, unixTime);
+
+    let text = `Current temp in ${city} is ${temp}`;
+    changeHTML(text)
   } catch (e) {
     console.error(e);
   }
 }
 
-let last_name = "Shin"
-let name = `Ben ${last_name}`
+function changeHTML(text) {
+  let op = document.getElementById("info");
+  op.style.display = "block";
+  let parent = document.getElementById("header");
+  op.innerHTML =
+  `
+  <div>
+    <p id="apple">${text}</p>
+  </div>
+  `;
+  parent.append(op);
+}
+
+
+
+// link("Seattle");
 // button.addEventListener("click", alert("lakjflasj"));
 // http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=98d4e0ae686e42c873d2a0575825fe83
 // https://api.openweathermap.org/data/2.5/weather?lat=51&lon=0&appid=9f23b56e8dcad8299bf4e5a2a3fc932b&units=metric
