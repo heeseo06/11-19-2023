ef3d8ed6e42ac5e32d9b31b811bfc0515f5bbc7d
seunguk <seunguk@cs.washington.edu>
2024-01-05 13:26:39 -0800

    cleanup

--- public/script.js
+++ public/script.js
@@ -36,8 +36,9 @@ async function link(city) {
   }
 }
 
-let last_name = "Shin"
-let name = `Ben ${last_name}`
-// button.addEventListener("click", alert("lakjflasj"));
+
+
+
+
 // http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=98d4e0ae686e42c873d2a0575825fe83
 // https://api.openweathermap.org/data/2.5/weather?lat=51&lon=0&appid=9f23b56e8dcad8299bf4e5a2a3fc932b&units=metric
--- script.js
+++ script.js
@@ -51,7 +51,7 @@ async function link(city) {
 
     let temp_feel = data["main"]["feels_like"];
 
-
+    // console.log(formatDate(today));
     let text = `Temperature in ${city}: ${temp}°C
     <br> Weather: ${description}`;
 
@@ -97,7 +97,6 @@ function changeHTML(text) {
 
   parent.append(op);
 }
-
 
 // http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=98d4e0ae686e42c873d2a0575825fe83
 // https://api.openweathermap.org/data/2.5/weather?lat=51&lon=0&appid=9f23b56e8dcad8299bf4e5a2a3fc932b&units=metric
